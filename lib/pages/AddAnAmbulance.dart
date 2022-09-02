90f866c7b9ed5c7416547535e6f00d941892a822
janep <bp5421@srmist.edu.in>
2022-09-02 18:44:25 +0530

    Added floating acrion button in firebase

---
+++ lib/pages/AddAnAmbulance.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AddAnAmbulance extends StatefulWidget {
+  const AddAnAmbulance({Key? key}) : super(key: key);
+
+  @override
+  State<AddAnAmbulance> createState() => _AddAnAmbulanceState();
+}
+
+class _AddAnAmbulanceState extends State<AddAnAmbulance> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,7 @@
+import 'package:dsc_hacks_technyts/pages/AddAnAmbulance.dart';
+import 'package:dsc_hacks_technyts/utils/colors.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +13,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.mainColor,
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddAnAmbulance()));
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
